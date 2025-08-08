bcb54f315a9ca3ecd06740fe51a61f3896e0f591
RoucheOelofse <st10321098@vcconnect.edu.za>
2025-08-08 14:55:43 +0200

    Add project files.

---
+++ prjInventoryManagement/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjInventoryManagement
+{
+    public class Product //Custom types
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int quantity { get; set; }
+        public decimal price { get; set; }
+    }
+    public static class ProductExtention //Extention method
+    {
+        public static decimal CalculateTotalValue(this List<Product> products)
+        {
+            //Calculate the total value
+            return products.Sum(p => p.quantity * p.price);
+        }
+
+        public static List<Product> FilterLowStockProducts(this List<Product> products, int lowStock)
+        {
+            //Filter products that are low in stock
+            return products.Where(p => p.quantity <= lowStock).ToList();
+        }
+    }
+}
+/* Reference List: 
+   C# corner, 2023. Extention methods. [Online] Available at: https://www.c-sharpcorner.com/UploadFile/puranindia/extension-methods-in-C-Sharp-3-0 [Accessed on: 08 August 2025]
+   Tutorialspoints, 2025, Explain cutom types. [Online] Available at: https://www.tutorialspoint.com/explain-the-custom-value-types-in-net [Accessed on: 08 August 2025]
+   
+ */
---
+++ prjInventoryManagement/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace prjInventoryManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product> //Anonymouse types
+            {
+                new Product{Id = 1, Name ="Water", quantity = 10, price = 50},
+                new Product{Id = 2, Name ="Kfc", quantity = 23, price = 150},
+                new Product{Id = 3, Name ="Pizza", quantity = 12, price = 30},
+            };
+            //LinQ to select product names & prices
+            var productNamesAndPrice = products.Select(p => new { p.Name, p.price, p.quantity }).ToList();
+
+            //Iterate anonymouse types to print results
+            foreach (var product in productNamesAndPrice)
+            {
+                Console.WriteLine($"Name: {product.Name}, Price: {product.price}, Quantity: {product.quantity}");
+            }
+
+            decimal totalValue = products.CalculateTotalValue();
+            Console.WriteLine($"Total Value: {totalValue}");
+
+            //Method to check the low stock product
+            List<Product> lowStockProducts = products.FilterLowStockProducts(10);
+            foreach (var product in lowStockProducts)
+            {
+                Console.WriteLine($"Low in stock: {product.Name}");
+            }
+        }
+    }
+}
+/* Reference List: 
+   Microsoft Learn, 2025. Anonymouse types. [Online] Available at: https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/types/anonymous-types [Accessed on: 08 August 2025]
+   C# corner, 2023. Extention methods. [Online] Available at: https://www.c-sharpcorner.com/UploadFile/puranindia/extension-methods-in-C-Sharp-3-0 [Accessed on: 08 August 2025]
+ */
